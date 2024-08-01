7a91c5404760dc00188e1bb8e09ccf47a410a5b8
keelyb <btkeely@gmail.com>
2024-07-31 22:14:00 -0400

    Update server.js
    Added AudioBooks and Videos to the sample data.
    Added require cors and cors logic.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const cors = require('cors');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -9,6 +10,8 @@ const schema = buildSchema(`
     id: ID!
     name: String!
     books: [Book!]!
+    audiobooks: [AudioBook!]!
+    videos: [Video!]!
   }
   
   type Book {
@@ -23,7 +26,24 @@ const schema = buildSchema(`
     authors: [Author!]!
     books: [Book!]!
   }
+
+  type AudioBook {
+    id: ID
+    title: String
+    durationMinutes: Int
+    author: Author
+    }
+
+  type Video {
+    id: ID
+    title: String
+    durationMinutes: Int
+    author: Author
+  }
 `);
+
+
+
 
 // The root provides a resolver function for each API endpoint
 const root = {
@@ -38,6 +58,14 @@ const root = {
           { id: 1, title: 'Harry Potter and the Sorcerer\'s Stone', pages: 309 },
           { id: 2, title: 'Harry Potter and the Chamber of Secrets', pages: 341 },
         ],
+        audiobooks: [
+          { id: 1, title: 'Harry Potter and the Sorcerer\'s Stone',  durationMinutes: 435 },
+          { id: 2, title: 'Harry Potter and the Chamber of Secrets',  durationMinutes: 440 },
+        ],
+        videos: [
+          { id: 1, title: 'Harry Potter and the Sorcerer\'s Stone',  durationMinutes: 135 },
+          { id: 2, title: 'Harry Potter and the Chamber of Secrets',  durationMinutes: 140 },
+        ],
       },
       {
         id: 2,
@@ -46,11 +74,19 @@ const root = {
           { id: 3, title: 'A Game of Thrones', pages: 694 },
           { id: 4, title: 'A Clash of Kings', pages: 768 },
         ],
+        audiobooks: [
+          { id: 3, title: 'A Game of Thrones',  durationMinutes: 435 },
+          { id: 4, title: 'A Clash of Kings',  durationMinutes: 440 },
+        ],
+        videos: [
+          { id: 3, title: 'A Game of Thrones',  durationMinutes: 145 },
+          { id: 4, title: 'A Clash of Kings',  durationMinutes: 150 },
+        ],
       },
     ];
     return authors;
   },
-  books: () => {
+  book: () => {
     // Fetch books data from a data source (e.g., database)
     const books = [
       { id: 1, title: 'Harry Potter and the Sorcerer\'s Stone', author: 1, pages: 309 },
@@ -60,13 +96,43 @@ const root = {
     ];
     return books;
   },
+  audiobook: () => {
+    const audiobooks = [
+      { id: 1, title: 'Harry Potter and the Sorcerer\'s Stone', author: 1, durationMinutes: 435 },
+      { id: 2, title: 'Harry Potter and the Chamber of Secrets', author: 1,  durationMinutes: 440 },
+      { id: 3, title: 'A Game of Thrones', author: 2,  durationMinutes: 445 },
+      { id: 4, title: 'A Clash of Kings', author: 2,  durationMinutes: 450 },
+    ];
+  },
+  videos: () => {
+    const videos = [
+      { id: 1, title: 'Harry Potter and the Sorcerer\'s Stone', author: 1, durationMinutes: 135 },
+      { id: 2, title: 'Harry Potter and the Chamber of Secrets', author: 1,  durationMinutes: 140 },
+      { id: 3, title: 'A Game of Thrones', author: 2,  durationMinutes: 145 },
+      { id: 4, title: 'A Clash of Kings', author: 2,  durationMinutes: 150 },
+    ];
+  },
 };
 
+const allowedOriginRegex = /^https?:\/\/localhost(:\d+)?$/;
+
 const app = express();
+app.use(cors({
+  // origin: 'http://localhost:3000', // Replace with the allowed origin(s)
+  origin: allowedOriginRegex,
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  maxAge: 3600 // 1 hour
+}));
+
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true
 }));
+
+
 
 app.listen(4000, () => console.log('Server running on port 4000'));
